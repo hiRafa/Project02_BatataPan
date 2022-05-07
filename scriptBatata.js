eaae2ab9a76d691dd6798056a16dddd2b594a5c8
HIRASHIKI RAFAEL <rafaelkishihiro@gmail.com>
2022-05-07 14:45:52 +0900

    Shops page (not completed yet)

--- scriptBatata.js
+++ scriptBatata.js
@@ -39,14 +39,16 @@ console.log(h1.parentElement.children);
 const hoverFade = function (opacity) {
   return function (e) {
     if (e.target.classList.contains('navi__a')) {
-      const link = e.target;
-      const siblings = link.closest('.navi__nav').querySelectorAll('.navi__a');
-      const logo = link.closest('.navi__nav').querySelector('img');
+      const target = e.target;
+      const siblings = target
+        .closest('.navi__nav')
+        .querySelectorAll('.navi__a');
+      const logo = target.closest('.navi__nav').querySelector('img');
 
       siblings.forEach(el => {
-        if (el !== link) el.style.opacity = opacity;
+        if (el !== target) el.style.opacity = opacity;
+        logo.style.opacity = opacity;
       });
-      logo.style.opacity = opacity;
     }
   };
 };
@@ -75,66 +77,71 @@ nav.addEventListener('mouseout', hoverFade(1));
 
 //-----------------------   -------------------------//
 
-//-----------------------   -------------------------//
+//----------------------- Section Observer   -------------------------//
 const allSections = document.querySelectorAll('.section');
 
-const revealSection = function (entries, observer) {
+const revealSection = function (entries) {
   const [entry] = entries;
   // console.log(entry);
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove('section-hidden');
+  if (!entry.isIntersecting) return; // when a section it not showing, this function returns here.
+  entry.target.classList.remove('section-hidden'); // when a new section is showing, this function applies removes the section hidden class
 };
 
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
   threshold: 0.15,
 });
+//function to observe the whole page and all sections and implement the revealSection function.
+
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section-hidden');
 });
+//this allows to add section hidden to all sections automatically when the page is loaded, without the need to add manually on the HTML
+//
+//
+//
 
-const imgHQ = document.querySelectorAll('img[data-src]');
+//----------------------- Lazy Load Image  -------------------------//
+const imgHQ = document.querySelectorAll('img[data-src]'); // [data-src] is for the original HQ img. (src was used for LQ version)
 const loadImg = function (entries, observer) {
   entries.forEach(entry => {
     // console.log(entry);
     if (!entry.isIntersecting) return;
 
-    // Replace src with data-src
-    entry.target.src = entry.target.dataset.src;
-
+    entry.target.src = entry.target.dataset.src; // Replace src with data-src
     entry.target.addEventListener('load', function () {
-      entry.target.classList.remove('lazy-img');
+      entry.target.classList.remove('lazy-img'); // lazy-img class has blur effect
     });
-    observer.unobserve(entry.target);
+    observer.unobserve(entry.target); //unobserve to stop any possible effect and consume less memory I think
   });
 };
 
 const imgObserver = new IntersectionObserver(loadImg, {
-  root: null,
+  root: null, // the container, the body, anything
   threshold: 0,
-  rootMargin: '-200px',
+  rootMargin: '-100px',
 });
 
 imgHQ.forEach(img => imgObserver.observe(img));
 
-//  ------------------  Slider
+//-----------------------  Slider  -------------------------//
 
 // const slider = document.querySelector('.section__news_carousel');
 // slider.style.overflow = 'visible';
 
 const sliderFunction = function () {
-  const slides = document.querySelectorAll('.section__news_carousel_slide');
+  const allSlides = document.querySelectorAll('.carousel_slide');
+  let currentSlide = 0;
+  const maxSlide = allSlides.length;
 
   const btnLeft = document.querySelector('.slider__btn--left');
   const btnRight = document.querySelector('.slider__btn--right');
   const dotContainer = document.querySelector('.dots');
 
-  let currentSlide = 0;
-  const maxSlide = slides.length;
-
+  // DOTs -------
   const createDots = function () {
-    slides.forEach(function (_, i) {
+    allSlides.forEach(function (_, i) {
       dotContainer.insertAdjacentHTML(
         'beforeend',
         `<button class="dots-dot" data-slide="${i}"></button>`
@@ -151,29 +158,31 @@ const sliderFunction = function () {
       .classList.add('dots-dot--active');
   };
 
+  // Slides -------
   const goToSlide = function (slide) {
-    slides.forEach(
-      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    allSlides.forEach(
+      (eachSlide, i) =>
+        (eachSlide.style.transform = `translateX(${100 * (i - slide)}%)`)
     );
   };
 
   const slideForward = function () {
     if (currentSlide === maxSlide - 1) {
-      currentSlide = 0;
+      currentSlide = 0; // to avoid slide forward to infinity and loop back to the first slide
     } else {
       currentSlide++;
     }
-    goToSlide(currentSlide);
+    goToSlide(currentSlide); // applying translateX
     activateDot(currentSlide);
   };
 
   const slideBackward = function () {
     if (currentSlide === 0) {
-      currentSlide = maxSlide - 1;
+      currentSlide = maxSlide - 1; // to avoid slide backward to infinity and loop back to the last slide
     } else {
       currentSlide--;
     }
-    goToSlide(currentSlide);
+    goToSlide(currentSlide); // applying translateX
     activateDot(currentSlide);
   };
 
@@ -181,8 +190,8 @@ const sliderFunction = function () {
   btnLeft.addEventListener('click', slideBackward);
 
   document.addEventListener('keydown', function (e) {
-    if (e.key === 'ArrowLeft') slideBackward();
-    e.key === 'ArrowRight' && slideForward();
+    if (e.key === 'ArrowLeft') slideBackward(); // different ways to write the same code
+    e.key === 'ArrowRight' && slideForward(); // different ways to write the same code
   });
 
   // ----------- Dots functions
@@ -192,6 +201,7 @@ const sliderFunction = function () {
     activateDot(0);
   };
   initializeSliderDots();
+  console.log(initializeSliderDots);
 
   dotContainer.addEventListener('click', function (e) {
     if (e.target.classList.contains('dots-dot')) {
